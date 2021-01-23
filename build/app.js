6c69f4e6b2f3d0ccb337a3519d2dbcb650dd6abe
andrey <andrey.batalev@gmail.com>
2021-01-23 20:16:10 +0300

    added use_json_server

--- build/app.js
+++ build/app.js
@@ -1,34 +1,52 @@
+// parameters
 const port = 8080;
 const json_port = 3002;
-const db_file = "db.json";
+const db_name = "../data/db.json";
+const js_path = "dist";
+const use_json_server = true;
+const real_backend = "http://backend:8080/";
 
+// launch
 const path = require("path");
+const db_path = path.join(__dirname, db_name);
+
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 
 server.use(jsonServer.defaults());
-server.use(jsonServer.router(path.join(__dirname, "../data/db.json")));
-server.listen(json_port, () => {
-  console.log("JSON Server is running");
-});
+server.use(jsonServer.router(db_path));
 
-//
 const { createProxyMiddleware } = require("http-proxy-middleware");
 const express = require("express");
 const app = express();
 
-app.use(express.static("dist"));
-app.use(
-  "/api",
-  createProxyMiddleware({
-    target: `http://localhost:${json_port}/`,
-    changeOrigin: true,
-    pathRewrite: {
-      [`^/api`]: "",
-    },
-  })
-);
+app.use(express.static(js_path));
+if (use_json_server) {
+  server.listen(json_port, () => {
+    console.log(`[INFO] JSON Server is running at http://localhost:${json_port}`);
+  });
+  console.log("[INFO] /api redirect to json-server");
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: `http://localhost:${json_port}/`,
+      changeOrigin: true,
+      pathRewrite: {
+        [`^/api`]: "",
+      },
+    })
+  );
+} else {
+  console.log("[INFO] /api redirect to backend: " + real_backend);
+  app.use(
+    "/api",
+    createProxyMiddleware({
+      target: real_backend,
+      changeOrigin: true,
+    })
+  );
+}
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+  console.log(`[INFO] App listening at http://localhost:${port}`);
 });
